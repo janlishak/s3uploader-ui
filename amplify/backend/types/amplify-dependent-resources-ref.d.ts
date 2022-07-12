2c970542c0d1983692ac9cb993fe7d681df7711a
EC2 Default User <ec2-user@ip-172-31-6-135.eu-central-1.compute.internal>
2022-07-12 14:22:19 +0000

    config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "s3uploader1162b51b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s31f925ece": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
